08e4733fd1a205a685ac0585895e0ed2456d8820
Pedro <pedrohenriqueacruz@gmail.com>
2012-03-26 08:48:10 -0300

    showtube

--- app/helpers/showtube/application_helper.rb
+++ app/helpers/showtube/application_helper.rb
@@ -1,4 +1,14 @@
 module Showtube
   module ApplicationHelper
+    #youtube videos
+    def youtube_tag(vid, shwidth, shheight, html_options = {})
+      %{<iframe width="#{shwidth}" height="#{shheight}" src="http://www.youtube.com/embed/#{vid}" frameborder="0" allowfullscreen></iframe>}.html_safe
+    end
+    
+    # imagem de video Youtube
+    def showtube_img_tag(vid, imwidth, imheight, html_options = {})
+      image_tag("http://img.youtube.com/vi/#{vid}/0.jpg", :size => "#{imwidth}x#{imheight}")
+    end
+  
   end
 end
--- db/migrate/20120325221502_create_showtube_videos.rb
+++ db/migrate/20120325221502_create_showtube_videos.rb
@@ -3,6 +3,10 @@ class CreateShowtubeVideos < ActiveRecord::Migration
     create_table :showtube_videos do |t|
       t.string :title
       t.string :url
+      t.string :shwidth
+      t.string :shheight
+      t.string :imwidth
+      t.string :imheight
       t.text :description
 
       t.timestamps
